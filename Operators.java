31c6b29909c17df9f140b79ee064cf7bd193e91e
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-02-10 22:03:45 +0545

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators 
+{  
+    public static void main(String[] args) 
+    {
+  
+      // Declare and initialize variables
+        int num1 = 500;
+        int num2 = 100;
+        
+        // Using the + (addition) operator
+        int sum = num1 + num2;
+        System.out.println("The Sum is: "+sum);
+        
+        // Using the - (subtraction) operator
+        int diff = num1 - num2;
+        System.out.println("The Difference is: "+diff);
+        
+    }
+}
