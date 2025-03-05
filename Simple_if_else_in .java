6b839c669461e53e5268d340b8f801eff35bd313
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-03-05 22:20:39 +0545

    Create Simple_if_else_in .java

---
+++ Simple_if_else_in .java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Simple_if_else_in {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int a=1;
+        System.out.println("This the value of java is "+a);
+        int b=2;
+        int c= a+b;
+        System.out.println("This the addition of a and b where c is="+c);
+        if (a==b){
+            System.out.println("a and b value is same:");
+        }
+            else{
+            System.out.println("a and b value is not same:");
+        }
+        
+    }
+}
