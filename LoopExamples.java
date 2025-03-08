b1121a204e77b6edad9b12c0eb9a83a3a6fddf0b
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-03-08 20:24:54 +0545

    Update LoopExamples.java

--- LoopExamples.java
+++ LoopExamples.java
@@ -1,16 +1,16 @@
 public class LoopExamples {
 
     public static void main(String[] args) {
-        // Example array
+      
         int[] numbers = {1, 2, 3, 4, 5};
 
-        // 1. for loop
+     
         System.out.println("Using for loop:");
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Element at index " + i + ": " + numbers[i]);
         }
 
-        // 2. while loop
+       
         System.out.println("\nUsing while loop:");
         int j = 0;
         while (j < numbers.length) {
@@ -18,7 +18,7 @@ public class LoopExamples {
             j++;
         }
 
-        // 3. do-while loop
+       
         System.out.println("\nUsing do-while loop:");
         int k = 0;
         do {
@@ -26,7 +26,7 @@ public class LoopExamples {
             k++;
         } while (k < numbers.length);
 
-        // 4. Enhanced for loop (for-each loop)
+        
         System.out.println("\nUsing enhanced for loop (for-each loop):");
         for (int number : numbers) {
             System.out.println("Element: " + number);
