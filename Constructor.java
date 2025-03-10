42d2c451c73da1e055ec5ee7bf66f7b7df938fe9
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-03-10 22:59:03 +0545

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+class Constructor {
+    Constructor()
+    {
+        super();
+        System.out.println("Constructor Called");
+    }
+
+    public static void main(String[] args)
+    {
+        Constructor geek = new Constructor();
+    }
+}
