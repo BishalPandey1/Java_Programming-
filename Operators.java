3d62c7b1a4340f217d28d30c55ab05578741b467
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-03-16 23:02:58 +0545

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -13,6 +13,12 @@ public class Operators
         
         int diff = num1 - num2;
         System.out.println("The Difference is: "+diff);
-        
+        if (num1>0 && num2 > 0 ) {
+            System.out.println("The number are positive");   
+        }
+        else {
+            System.out.println("They are negative number");
+        }
+   
     }
 }
