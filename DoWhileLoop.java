8437788ca0507bc18e932727644ede93f1f8d184
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-02 21:04:53 +0545

    Create DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop{
+    public static void main(String[] args) {
+        System.out.println("\nUsing do-while loop:");
+        int i = 1;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 10);
+    }
+}
