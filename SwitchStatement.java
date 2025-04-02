e8e402e885f4e7a2c00a1d5f695d08d0dbbcdf34
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-02 21:02:04 +0545

    Create  SwitchStatement.java

---
+++ SwitchStatement.java
@@ -0,0 +1,47 @@
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for two numbers
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Prompt the user for an operation
+        System.out.println("Choose an operation: +, -, *, /");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        // Perform the operation using a switch statement
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+        }
+
+        scanner.close(); // Close the scanner
+    }
+}
