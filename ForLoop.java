1bc4054043bb7a5d37460c4ad24cdd52aba111aa
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-02 21:03:26 +0545

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Using for loop:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
