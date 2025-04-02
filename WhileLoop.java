033b19f5c65c6cbfc0493cbd5f312cdff117cb9d
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-02 21:04:08 +0545

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println("\nUsing while loop:");
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
