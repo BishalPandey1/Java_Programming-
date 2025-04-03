995cf2cf1f43dc573ee83ac6511115b7be738681
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-03 21:25:54 +0545

    Create AbstractClassExample.java

---
+++ AbstractClassExample.java
@@ -0,0 +1,36 @@
+// Abstract class
+abstract class Animal {
+    // Abstract method
+    abstract void makeSound();
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+    // Implementation of makeSound()
+    @Override
+    void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+}
+
+// Cat class extending Animal
+class Cat extends Animal {
+    // Implementation of makeSound()
+    @Override
+    void makeSound() {
+        System.out.println("Meow! Meow!");
+    }
+}
+
+// Main class to test the implementation
+public class AbstractClassExample {
+    public static void main(String[] args) {
+        // Create objects of Dog and Cat
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        // Call makeSound() for each object
+        myDog.makeSound();
+        myCat.makeSound();
+    }
+}
