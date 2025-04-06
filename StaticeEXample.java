526c4fc5a00476b0e79a32efba191553a78d7f34
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-06 21:38:39 +0545

    Update StaticeEXample.java

--- StaticeEXample.java
+++ StaticeEXample.java
@@ -1,14 +1,14 @@
 public class StaticExample {
-    // Static variable
+    
     static int studentCount = 0;
 
-    // Static method to increment and display the student count
+    
     public static void incrementStudentCount() {
         studentCount++;
         System.out.println("Student count: " + studentCount);
     }
 
-    // Main method
+    
     public static void main(String[] args) {
         System.out.println("Demonstrating static methods and variables:");
 
