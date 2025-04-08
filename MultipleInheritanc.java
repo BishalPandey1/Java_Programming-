a98b6c24b276dfcdea4bcc1b13927ff3e835adfe
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-08 22:49:29 +0545

    Create MultipleInheritanc.java

---
+++ MultipleInheritanc.java
@@ -0,0 +1,29 @@
+
+interface Animal {
+    void eat();
+}
+
+
+interface Bird {
+    void fly();
+}
+
+class Bat implements Animal, Bird {
+    @Override
+    public void eat() {
+        System.out.println("Bat is eating.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Bat is flying.");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Bat bat = new Bat();
+        bat.eat();
+        bat.fly();
+    }
+}
