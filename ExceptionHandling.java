859d07cfa72314b7bdc9b3eac9b3a84f3d351620
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-04-25 22:20:27 +0545

    Create ExceptionHandling.java

---
+++ ExceptionHandling.java
@@ -0,0 +1,16 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try {
+            double result = 1.0 / 0.0; 
+            
+            if (Double.isInfinite(result)) {
+                throw new ArithmeticException("Infinity detected!");
+            }
+            
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Cannot work with infinity!");
+        }
+    }
+}
