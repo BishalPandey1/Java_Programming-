90c2b0fc5175d35357c8d900d8ca2f1098252518
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-07-21 21:28:05 +0545

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,13 @@
+// Java Program to demonstrate String
+public class Strings {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String("Geeks");
+
+        System.out.println(str);
+    }
+}
