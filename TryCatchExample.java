e49a9ece6661455a35635e2f4cbf3210ddcbf55d
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-07-27 22:17:46 +0545

    Update TryCatchExample.java

--- TryCatchExample.java
+++ TryCatchExample.java
@@ -5,10 +5,12 @@ public class TryCatchExample {
             int b = 0;
             int result = a / b; 
             System.out.println("Result: " + result);
+            // In try we can find where error can be occure
         } catch (ArithmeticException e) {
             System.out.println("Oops! You can't divide by zero.");
         }
 
         System.out.println("Program continues after the catch block.");
     }
+    // In catch we solve the problem wehere error can be occure and solve it.
 }
